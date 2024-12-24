4414638b254f1c9e1e5bd54bab1012756a394f32
TrueX <truexrevs@gmail.com>
2024-12-24 09:04:30 -0500

    introduced compose

--- intro.py
+++ intro.py
@@ -21,3 +21,56 @@ https://youtu.be/8vmKtS8W7IQ?si=J1DoOyVXydoBU-2J
 # docker images
 
 # docker run python:0.0.1
+# docker run --name python1 -p 80:80 python:0.0.1
+
+# use volumes to persist data from you local files (e.g python) to the container
+
+'''
+list all the running containers
+docker ps
+
+list all the containers (bothe running and stopped)
+docker ps --all 
+
+remove all containers
+docker rm $(docker ps --all -q)
+
+remove a container
+docker rm <CONTAINER_ID> or <CONTAINER NAME>
+
+remove an image (make sure the container has been removed first)
+docker rmi <IMAGE_ID>
+
+create your container, give it a name and transfer port from container to local host
+docker run --name <GIVE IT A NAME> -p 80:80 <IMAGE NAME>
+
+run the container in detach mode (-d) so it can run in the background 
+docker run --name <GIVE IT A NAME> -p 80:80 -d <IMAGE NAME>
+
+stop your container (especially if you have it running in the background)
+docker stop <CONTAINER NAME>
+
+use volumes (-v) to persist data and map your local working directory to your container working directory
+docker run --name <GIVE IT A NAME> -p 80:80 -d -v ${pwd}:/code <IMAGE NAME>
+
+since you ran your installations in the container and not in your local machine IDE, you have some issues 
+(like no code completions) when editing you python files for instance. so install your IDE in the container
+in VS code just add the "Docker" and "Dev containers" extensions and you are good.
+
+docker compose helps simplify the commands and flags we are using to run our container and we run only one command.
+docker-compose up
+docker-compose down
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
