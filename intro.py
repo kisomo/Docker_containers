8af8a43590b658b67552a3c2902a7c591e35ce3f
TrueX <truexrevs@gmail.com>
2024-12-23 21:54:58 -0500

    first container

---
+++ intro.py
@@ -0,0 +1,23 @@
+
+'''
+https://youtu.be/0H2miBK_gAk?si=QfjSd_WtlpFKePLh
+
+https://youtu.be/b0HMimUb4f0?si=7hb3nwBtoiZ0KJdx
+
+https://youtu.be/8vmKtS8W7IQ?si=J1DoOyVXydoBU-2J
+
+'''
+
+# python -m venv mycontainers
+# mycontainers\Scripts\Activate.ps1
+# python.exe -m pip install --upgrade pip
+# pip3 install -r requirements.txt
+# pip install ipykernel 
+
+# uvicorn main:app --host localhost --port 80
+
+# docker image build -t python:0.0.1 /Users/tmuth/MODELS/Docker_containers
+
+# docker images
+
+# docker run python:0.0.1
